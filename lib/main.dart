17fcef3fcbb78d0995af091a668627d0021d1986
Gabriel Donato <gabrielcdonato@gmail.com>
2021-11-20 17:30:15 -0300

    primeira etapa da pokedex

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:pokedexflutter/common/repositories/pokemon_repository.dart';
+
+import 'features/home/container/home_container.dart';
+
+// entre () sao parametros
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Pokedex',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomeContainer(
+          repository: PokemonRepository(),
+        ));
+  }
+}
