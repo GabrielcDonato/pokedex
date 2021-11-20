91469d3fcb5d646ecbff6fac50ae22936cdde03f
Gabriel Donato <gabrielcdonato@gmail.com>
2021-11-20 17:59:09 -0300

    Second Step: Response api

--- lib/features/home/container/home_container.dart
+++ lib/features/home/container/home_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pokedexflutter/common/errors/failure.dart';
 import 'package:pokedexflutter/common/models/pokemon.dart';
 import 'package:pokedexflutter/common/repositories/pokemon_repository.dart';
 import 'package:pokedexflutter/features/home/pages/home_error.dart';
@@ -24,7 +25,7 @@ class HomeContainer extends StatelessWidget {
 
           if (snapshot.hasError) {
             return HomeError(
-              error: snapshot.error.toString(),
+              error: (snapshot.error as Failure).message!,
             );
           }
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:pokedexflutter/common/repositories/pokemon_repository.dart';
 
@@ -12,12 +13,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Pokedex',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
+      title: 'Pokedex',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeContainer(
+        repository: PokemonRepository(
+          dio: Dio(),
         ),
-        home: HomeContainer(
-          repository: PokemonRepository(),
-        ));
+      ),
+    );
   }
 }
