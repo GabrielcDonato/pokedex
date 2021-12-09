0b93aecdedf07d0f02e4e3ddc039597ba9708f9f
gabriel <gabrielcdonato@gmail.com>
2021-12-09 00:14:14 -0300

    feat: appbar implementation with details

--- lib/features/pokedex/screens/home/pages/home_page.dart
+++ lib/features/pokedex/screens/home/pages/home_page.dart
@@ -10,6 +10,33 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        centerTitle: false,
+        title: Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: Text(
+            'Pokedex',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 26,
+            ),
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.menu,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ],
+      ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 24),
         child: GridView.count(
